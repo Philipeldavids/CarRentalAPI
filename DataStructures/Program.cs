89e0acb88038be410b4c4b769ecf8946bfff1565
ChisomOkonkwoC <chisom.okonkwo@decagon.dev>
2022-05-21 16:16:41 +0100

    added some changes to the get all cars by page number

---
+++ DataStructures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Program
+    {
+        public static int[] PrintArray()
+        {
+            int[] numbers = new int[4 ];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            numbers[3] = 40;
+            return numbers;
+        }
+        static void Main(string[] args)
+        {
+            var result = PrintArray();
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
