31ae754c6d84e280a2e0b327df2292acbefa1fd9
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-25 07:50:39 +0100

    created dealer repo and interface

---
+++ RentalCarInfrastructure/Repositories/Implementations/DealerRepository.cs
@@ -0,0 +1,24 @@
+﻿using RentalCarInfrastructure.Context;
+using RentalCarInfrastructure.Models;
+using RentalCarInfrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCarInfrastructure.Repositories.Implementations
+{
+    public class DealerRepository : GenericRepository<DealerRepository>, IDealerRepository
+    {
+        public DealerRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Dealer>> GetDealersAsync()
+        {
+            var dealers = await GetAllRecord();
+            return (List<Dealer>)dealers;
+        }
+    }
+}
